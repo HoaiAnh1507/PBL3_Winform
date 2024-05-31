841bd1cfe991fa6f25fa8bf1ced4cc95a8784867
HoaiAnh1507 <hoaianhnguyen277@gmail.com>
2024-06-01 06:11:48 +0700

    doimk

---
+++ DoHoaC#/QLTaiKhoan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoHoaC_
+{
+    public class QLTaiKhoan
+    {
+        private static QLTaiKhoan _Instance;
+        public static QLTaiKhoan Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                    _Instance = new QLTaiKhoan();
+                return _Instance;
+            }
+            private set { }
+        }
+        public void ChangePassword(DTB_TaiKhoan acc, string newPass)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+                {
+                    string query = "UPDATE TAIKHOAN SET MatKhau = @NewMatKhau WHERE TenTaiKhoan = @TenTaiKhoan AND MatKhau = @OldMatKhau";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@NewMatKhau", newPass);
+                    command.Parameters.AddWithValue("@TenTaiKhoan", acc.TenTaiKhoan);
+                    command.Parameters.AddWithValue("@OldMatKhau", acc.MatKhau);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected == 0)
+                        throw new InvalidOperationException("Mật khẩu cũ không đúng hoặc không tìm thấy tài khoản để cập nhật!");
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Đã xảy ra lỗi khi đổi mật khẩu!", ex);
+            }
+        }
+    }
+}
---
+++ DoHoaC#/QLThongKe.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DoHoaC_
+{
+    public class QLThongKe
+    {
+        private static QLThongKe _Instance;
+        public static QLThongKe Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                    _Instance = new QLThongKe();
+                return _Instance;
+            }
+            private set { }
+        }
+
+        private QLThongKe() { }
+
+        public DataTable ThongkeByDate(DateTime startDate, DateTime endDate, string a)
+        {
+            DataTable dataTable = new DataTable();
+            string query;
+
+            if (a == "NhanVien")
+            {
+                query = "SELECT ID_NV, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE NGAY_MUA BETWEEN @startDate AND @endDate GROUP BY ID_NV;";
+            }
+            else if (a == "KhachHang")
+            {
+                query = "SELECT ID_KH, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE NGAY_MUA BETWEEN @startDate AND @endDate GROUP BY ID_KH;";
+            }
+            else // Tổng doanh thu
+            {
+                query = "SELECT NGAY_MUA, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE NGAY_MUA BETWEEN @startDate AND @endDate GROUP BY NGAY_MUA;";
+            }
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@startDate", startDate);
+                    command.Parameters.AddWithValue("@endDate", endDate);
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        public DataTable ThongkeByMonth(int month, string a)
+        {
+            DataTable dataTable = new DataTable();
+            string query;
+
+            if (a == "NhanVien")
+            {
+                query = "SELECT ID_NV, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE MONTH(NGAY_MUA) = @month GROUP BY ID_NV;";
+            }
+            else if (a == "KhachHang")
+            {
+                query = "SELECT ID_KH, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE MONTH(NGAY_MUA) = @month GROUP BY ID_KH;";
+            }
+            else // Tổng doanh thu
+            {
+                query = "SELECT DAY(NGAY_MUA) AS Ngay, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE MONTH(NGAY_MUA) = @month GROUP BY DAY(NGAY_MUA);";
+            }
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@month", month);
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        public DataTable ThongkeByYear(int year, string a)
+        {
+            DataTable dataTable = new DataTable();
+            string query;
+
+            if (a == "NhanVien")
+            {
+                query = "SELECT ID_NV, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE YEAR(NGAY_MUA) = @year GROUP BY ID_NV;";
+            }
+            else if (a == "KhachHang")
+            {
+                query = "SELECT ID_KH, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE YEAR(NGAY_MUA) = @year GROUP BY ID_KH;";
+            }
+            else // Tổng doanh thu
+            {
+                query = "SELECT MONTH(NGAY_MUA) AS Thang, SUM(TONG_THANH_TOAN) AS DoanhThu FROM DONHANG WHERE YEAR(NGAY_MUA) = @year GROUP BY MONTH(NGAY_MUA);";
+            }
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@year", year);
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+    }
+}
