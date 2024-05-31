956a30a4bbb8e22abc4bab2d803d409f93391da0
HoaiAnh1507 <hoaianhnguyen277@gmail.com>
2024-06-01 04:15:49 +0700

    sl

---
+++ DoHoaC#/DTB_TaiKhoan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoHoaC_
+{
+    public class DTB_TaiKhoan
+    {
+        private string tenTaiKhoan;
+        public string TenTaiKhoan
+        {
+            get => tenTaiKhoan;
+            set => tenTaiKhoan = value;
+        }
+        private string matKhau;
+        public string MatKhau
+        {
+            get => matKhau;
+            set => matKhau = value;
+        }
+        private bool loaiTaiKhoan;
+        public bool LoaiTaiKhoan
+        {
+            get => loaiTaiKhoan;
+            set => loaiTaiKhoan = value;
+        }
+        public DTB_TaiKhoan(string tentaikhoan, string matkhau, bool loaiTaiKhoan)
+        {
+            this.TenTaiKhoan = tentaikhoan;
+            this.MatKhau = matkhau;
+            this.LoaiTaiKhoan = loaiTaiKhoan;
+        }
+    }
+}
---
+++ DoHoaC#/FormDoiMK.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoHoaC_
+{
+    public partial class FormDoiMK : Form
+    {
+        public FormDoiMK()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonLuuMK_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
